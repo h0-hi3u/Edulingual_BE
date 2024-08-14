e8505e6a09edec874e6d5b30cf8eb2155ceb53a6
h0-hi3u <hieuenglishabc@gmail.com>
2024-08-14 12:19:48 +0700

    [Exception] handle some exception

---
+++ Edlingual.Common/Exceptions/MissingConnectionStringException.cs
@@ -0,0 +1,18 @@
+﻿namespace Edulingual.Common.Exceptions;
+
+public class MissingConnectionStringException : ArgumentNullException
+{
+    private readonly string? _customMessage;
+
+    public override string Message => _customMessage ?? Message;
+
+    public MissingConnectionStringException(string customMessage)
+    {
+        _customMessage = customMessage;
+    }
+
+    public MissingConnectionStringException() 
+    {
+        _customMessage = "Can not find connection string";
+    }
+}
---
+++ Edulingual.Service/Exceptions/DatabaseException.cs
@@ -0,0 +1,20 @@
+﻿using Edulingual.Common.Exceptions;
+
+namespace Edulingual.Service.Exceptions;
+
+public class DatabaseException : Exception, IDatabaseException
+{
+    private readonly string _customMessage;
+
+    public override string Message => _customMessage ?? Message;
+
+    public DatabaseException(string customMessage)
+    {
+        _customMessage = customMessage;
+    }
+
+    public DatabaseException() 
+    {
+        _customMessage = "Internal Server Error!";
+    }
+}
