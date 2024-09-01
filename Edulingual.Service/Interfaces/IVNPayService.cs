ed00fcf9875b1d39563ac5cabc1df0a2d6be3162
h0-hi3u <hieuenglishabc@gmail.com>
2024-09-01 13:35:25 +0700

    [VNPay] setup interface and library

---
+++ Edulingual.Service/Interfaces/IVNPayService.cs
@@ -0,0 +1,9 @@
+﻿using Edulingual.Service.Models;
+
+namespace Edulingual.Service.Interfaces;
+
+public interface IVNPayService
+{
+    string CreatePaymentUrl(int amount);
+    Task<ServiceActionResult> CreatePaymentVNPay(Guid userId, int amount, Guid courseId);
+}
---
+++ Edulingual.Service/Library/VNPayModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Edulingual.Service.Library;
+
+public class VNPayModel
+{
+    public string TmnCode { get; set; }
+    public string HashSecret { get; set; }
+    public string Url { get; set; }
+    public string ReturnUrl { get; set; }
+    public string CancelUrl { get; set; }
+}
